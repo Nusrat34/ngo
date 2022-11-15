d2306c567ef1e577ce6925417dd7fc7fd6697d4e
nusrat34 <nusjahan244@gmail.com>
2022-11-15 17:06:55 +0600

    login

--- resources/views/Backend/Partials/sidebar.blade.php
+++ resources/views/Backend/Partials/sidebar.blade.php
@@ -27,6 +27,7 @@
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
                     loan
                 
+                
                 </a>
                 <a class="nav-link" href="{{ url('member')}}">
                     <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,7 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\BookController;
 use App\Http\Controllers\MemberController;
 use App\Http\Controllers\NoticeController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,6 +21,12 @@ Route::get('/', function () {
     return view('backend.master');
 });
 
+
+
+Route::get('/login',[UserController::class,'login'])->name('login');
+
+
+
 Route::get('/book',[BookController::class,'book']);
 Route::get('book/bookform',[BookController::class,'bookform']);
 Route::post('book/bookform/store',[BookController::class,'store'])->name('book.bookform.store');
@@ -31,7 +38,6 @@ Route::post('/member/memberform',[MemberController::class,'store'])->name('membe
 Route ::get('/notice',[NoticeController::class,'notice']);
 Route ::get('/notice/noticeform',[NoticeController::class,'noticeform']);
 Route ::post('/notice/noticeform/store',[NoticeController::class,'store'])->name('notice.noticeform.store');
-
 Route::get('/dashboard',[HomeController::class,'dashboard'])->name('dashboard');
 
 
